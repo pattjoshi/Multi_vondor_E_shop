6a3b2a03b6c9689b685ec9d251657dcefd942976
pattjoshi <omprakashpattjoshi123@gmail.com>
2023-07-17 18:30:26 +0530

    upd

--- frontend/src/pages/Shop/ShopPreviewPage.jsx
+++ frontend/src/pages/Shop/ShopPreviewPage.jsx
@@ -6,7 +6,7 @@ import ShopProfileData from "../../components/Shop/ShopProfileData";
 const ShopPreviewPage = () => {
   return (
     <div className={`${styles.section} bg-[#f5f5f5]`}>
-      <div className="w-full 800:flex py-10 justify-between">
+      <div className="w-full 800px:flex py-10 justify-between">
         <div className="800px:w-[25%] bg-[#fff] rounded-[4px] shadow-sm 800px:overflow-y-scroll 800px:h-[90vh] 800px:sticky top-10 left-0 z-10">
           <ShopInfo isOwner={false} />
         </div>
